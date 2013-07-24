70cd52cd84d8cd1994200351580495b418f69393
deant <dean@omnivisiontechnology.com>
2013-07-24 13:38:43 +1000

    Oops - my last update assumed the JSON contained "imageUrl" and "thumbUrl".
    Of course, it should just be "image" and "thumb".
    
    (The other recognized properties are "folder" and "label")

--- browser/browser.js
+++ browser/browser.js
@@ -2,7 +2,7 @@
 var IB = {
 
 	images : {},
-	
+
 	init : function() {
 		var head, thisdir, baseHref = IB.getQueryStringParam("baseHref");
 		if (baseHref) {
@@ -16,37 +16,37 @@ var IB = {
 		$("#container").on("click", "a", function() { window.opener.CKEDITOR.tools.callFunction(IB.getQueryStringParam("CKEditorFuncNum"), this.href);	window.close();	return false; });
 		$.getJSON(IB.getQueryStringParam("listUrl"), IB.processData);
 	},
-	
+
 	processData : function(list) {
 		var item, folder, i, l, count = 0, $ul = $("ul"), images = IB.images;
 		for (i=0, l = list.length; i<l; i++) {
 			var item = list[i];
 			var folder = ((!item.folder)?"Images":item.folder);
 			delete item.folder;
-			if (item.imageUrl) {
-				if (!item.thumbUrl) item.thumbUrl = item.imageUrl;
+			if (item.image) {
+				if (!item.thumb) item.thumb = item.image;
 				if (!item.label) item.label = ""; else item.label = "<pre>"+item.label+"</pre>";
 				if (images.hasOwnProperty(folder)) images[folder].push(item);
 				else {
 					images[folder] = [item];
 					$("<li></li>").text(folder).appendTo($ul);
 					count++;
-				} 
+				}
 			}
 		}
 		if (count>0) $($ul.get(0).firstChild).click(); else $('#container').html('<div style="text-align:center;margin-top:100px;font-size:24px"><i>No images found!</i></div>');
 		if (count<=1) { $ul.remove(); document.getElementById("container").style.top = "0"; }
-		$("#loadMsg").remove();		
+		$("#loadMsg").remove();
 	},
-	
+
 	renderImagesForFolder : function(folder) {
 		var i, l, img, images = IB.images[folder], $container = $("#container").html("");
 		for (i = 0, l = images.length; i<l; i++) {
 			img = images[i];
-			$container.append($.parseHTML('<a href="'+img.imageUrl+'"><img src="'+img.thumbUrl+'">'+img.label+'</a>'));
+			$container.append($.parseHTML('<a href="'+img.image+'"><img src="'+img.thumb+'">'+img.label+'</a>'));
 		}
 	},
-	
+
 	getQueryStringParam : function(name) {
 		var result = window.location.search.match(new RegExp("[?&]" + name + "=([^&]*)"));
 		return ((result && (result.length > 1)) ? decodeURIComponent(result[1]) : null);
