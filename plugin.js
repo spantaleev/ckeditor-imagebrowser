903623ea09e19344a65faa1cf99b545b7495ad7d
deant <dean@omnivisiontechnology.com>
2013-07-24 13:25:39 +1000

    Consistent double-quotes.

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
-CKEDITOR.plugins.add('imagebrowser', {
+CKEDITOR.plugins.add("imagebrowser", {
 	"init": function (editor) {
 		if (editor.config.imageBrowser_listUrl!=null) editor.config.filebrowserImageBrowseUrl = editor.plugins.imagebrowser.path + "browser/browser.html?listUrl=" + encodeURIComponent(editor.config.imageBrowser_listUrl) + (editor.config.baseHref ? "&baseHref=" + encodeURIComponent(editor.config.baseHref) : "");
 	}
