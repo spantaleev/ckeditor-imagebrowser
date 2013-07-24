28fb70a750efbaa4aa1302c1fbe2473070cd2b90
deant <dean@omnivisiontechnology.com>
2013-07-24 13:22:05 +1000

    Fix "undefined" in querystring.

--- plugin.js
+++ plugin.js
@@ -1,9 +1,5 @@
 CKEDITOR.plugins.add('imagebrowser', {
 	"init": function (editor) {
-		if (typeof(editor.config.imageBrowser_listUrl) === 'undefined' || editor.config.imageBrowser_listUrl === null) {
-			return;
-		}
-
-		editor.config.filebrowserImageBrowseUrl = editor.plugins.imagebrowser.path + "browser/browser.html?listUrl=" + encodeURIComponent(editor.config.imageBrowser_listUrl) + "&baseHref=" + encodeURIComponent(editor.config.baseHref);
+		if (editor.config.imageBrowser_listUrl!=null) editor.config.filebrowserImageBrowseUrl = editor.plugins.imagebrowser.path + "browser/browser.html?listUrl=" + encodeURIComponent(editor.config.imageBrowser_listUrl) + (editor.config.baseHref ? "&baseHref=" + encodeURIComponent(editor.config.baseHref) : "");
 	}
 });
